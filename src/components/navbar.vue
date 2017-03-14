7eb632800709486682acf582dce2db293a20edf9
ZickFz <zickfz@gmail.com>
2017-03-15 03:22:34 +0700

    Init commit

---
+++ src/components/navbar.vue
@@ -0,0 +1,20 @@
+<template lang="html">
+  <nav class="nav has-shadow" >
+    <div class="nav-left">
+      <router-link class="nav-item is-brand" to="/">
+        Home
+      </router-link>
+      <router-link class="nav-item is-tab" to="/dashboard">
+        <i class="fa fa-dashboard"></i> Dashboard
+      </router-link>
+      <router-link class="nav-item is-tab" to="/projects">
+        <i class="fa fa-list-alt"></i> Project List
+      </router-link>
+    </div>
+  </nav>
+</template>
+<script>
+export default {
+  name: 'navbar',
+};
+</script>
---
+++ src/containers/projects.vue
@@ -0,0 +1,50 @@
+<!-- projects -->
+<template lang="html">
+  <div id="projects">
+    <div class="columns">
+      <div class="column is-half">
+        <div class="notification">
+          Github repositories
+        </div>
+        <div class="">
+          <table class="table">
+            <thead>
+              <tr>
+                <th>Project Name</th>
+                <th>Url</th>
+                <th>Description</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="item in projects">
+                <td>{{item.name}}</td>
+                <td><a :href="item.html_url">See more</a></td>
+                <td>{{item.description}}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'projects',
+  data() {
+    return {
+      projects: [],
+    };
+  },
+  methods: {
+    loadProjects() {
+      this.axios.get('https://api.github.com/users/zickfz/repos').then((response) => {
+        this.projects = response.data;
+      });
+    },
+  },
+  mounted() {
+    this.loadProjects();
+  },
+};
+</script>
