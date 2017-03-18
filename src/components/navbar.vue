bc95d5fadf38fa02d43d7f88e9c1835c8e843991
ZickFz <zickfz@gmail.com>
2017-03-18 23:57:34 +0700

    change typer

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -2,13 +2,13 @@
   <nav class="nav has-shadow" >
     <div class="nav-left">
       <router-link class="nav-item is-brand" to="/">
-            <i class="fa fa-home"></i>Home
+            <i class="fa fa-home icon is-medium"></i>Home
       </router-link>
       <router-link class="nav-item is-tab" to="/dashboard">
-        <i class="fa fa-dashboard"></i> Dashboard
+        <i class="fa fa-dashboard icon is-medium"></i> Dashboard
       </router-link>
       <router-link class="nav-item is-tab" to="/projects">
-        <i class="fa fa-list-alt"></i> Project List
+        <i class="fa fa-list-alt icon is-medium"></i> Project List
       </router-link>
     </div>
   </nav>
--- src/containers/Home.vue
+++ src/containers/Home.vue
@@ -2,8 +2,7 @@
 <template lang="html">
   <div id="home">
      <h1 class="title is-1"><vue-typer text="Hello,"></vue-typer></h1>
-      <p class="subtitle is-2"><vue-typer text="My name is Pom. I'm a Developer living in Bangkok."></vue-typer></p>
-
+      <p class="subtitle is-2">My name is Pom. I'm a Developer living in Bangkok.</p>
   </div>
 </template>
 <script>
